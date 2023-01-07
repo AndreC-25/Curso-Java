f02a3a78129102d8b1c339a73a7b50a20cdbbaa0
AndreC-25 <andrecabrejos.25@gmail.com>
2023-01-06 23:47:57 -0500

    Ejercicio 1

---
+++ Proyecto-Ejercicio-01/src/com/ejercicio1/Main.java
@@ -0,0 +1,21 @@
+package com.ejercicio1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nombre = "André";
+        int edad = 28;
+        double estatura = 1.80;
+        float decimal = 5.46f;
+        long numero1 = 23;
+        short numero2 = 47;
+        byte numero3 = 8;
+        char caracter = 'b';
+        boolean respuesta = true;
+
+        System.out.println(nombre + ", " + edad + ", " + estatura + ", " + decimal + ", " + numero1 + ", " + numero2 + ", " + numero3 + ", " + caracter + ", " + respuesta);
+
+    }
+
+}
