9569577c45b7214c81950a71539fa64e5fb648bb
AndreC-25 <andrecabrejos.25@gmail.com>
2023-01-15 22:19:44 -0500

    Ejercicio 5

---
+++ Proyecto-Ejercicio-05/src/CocheCRUDImpl.java
@@ -0,0 +1,16 @@
+public class CocheCRUDImpl implements CocheCRUD {
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void findAll() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
